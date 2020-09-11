ab1c940d6e4fc68a89cc1a310e159647ac924849
jenross <ross.m.jennifer@gmail.com>
2020-09-11 19:33:13 -0400

    keyframes

--- src/App.js
+++ src/App.js
@@ -15,9 +15,8 @@ function App() {
   const [isToggled, setToggle] = useState(false);
   return (
     <motion.div
-      initial={{ opacity: 0 }}
-      animate={{ opacity: 1 }}
-      transition={{ duration: 1 }}
+      animate={{ opacity: [0, 1, 0, 1] }}
+      transition={{ duration: 5, times: [0, 0.2, 0.3, 1] }}
     >
       <Header>
         <Menu />
